6f5f28274be3b1bc33db2dfda312e29c31d46b92
AdamRoomka <adam.romas12@gmail.com>
2022-03-24 14:42:35 +0200

    without php

--- php/errorslogin.php
+++
@@ -1,7 +0,0 @@
-<?php  if (count($errorsLogin) > 0) : ?>
-  <div class="error">
-  	<?php foreach ($errorsLogin as $errorLogin) : ?>
-		<p class="text-danger text-center"><?php echo $errorLogin ?></p>
-  	<?php endforeach ?>
-  </div>
-<?php  endif ?>
